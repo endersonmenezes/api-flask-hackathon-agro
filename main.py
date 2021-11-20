3eb80e0e905ff56a3ace38f6f8b559f6b3abe8be
Enderson Menezes (Mr. Enderson) <endersonster@gmail.com>
2021-11-20 18:42:17 +0000

    atualizando pra usar como modulo

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ app = Flask('app')
 def hello_world():
   return 'Hello, World!'
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port)
