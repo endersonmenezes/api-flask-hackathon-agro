1b7045d44ab100ff2704f5b6a842f40bff1369f1
Enderson Menezes (Mr. Enderson) <endersonster@gmail.com>
2021-11-20 18:40:28 +0000

    update

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask
+import os
+
 app = Flask('app')
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
 
-app.run(host='0.0.0.0', port=8080)
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
